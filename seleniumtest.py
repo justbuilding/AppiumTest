a2753dee729348255073d5b2d5c475f77b9a2ee1
Jay Q <41616424+justbuilding@users.noreply.github.com>
2019-06-05 22:07:19 +0800

    test selenium

--- seleniumtest.py
+++ seleniumtest.py
@@ -1,3 +1,4 @@
+#
 # from selenium import webdriver
 #
 #
@@ -10,6 +11,7 @@
 
 
 
+#控制浏览器窗口大小
 # from selenium import webdriver
 #
 #
@@ -23,27 +25,69 @@
 
 
 
+#控制浏览器后退、前进
+# from selenium import webdriver
+#
+#
+# driver = webdriver.Chrome()
+#
+# #访问百度首页
+# first_url= 'http://www.baidu.com'
+# print("now access %s" %(first_url))
+# driver.get(first_url)
+#
+# #访问新闻页面
+# second_url='http://news.baidu.com'
+# print("now access %s" %(second_url))
+# driver.get(second_url)
+#
+# #返回（后退）到百度首页
+# print("back to  %s "%(first_url))
+# driver.back()
+#
+# #前进到新闻页
+# print("forward to  %s"%(second_url))
+# driver.forward()
+#
+# driver.quit()
+
+
+
+# #点击和输入
+# from selenium import webdriver
+#
+# driver = webdriver.Chrome()
+# driver.get("https://www.baidu.com")
+#
+# driver.find_element_by_id("kw").clear()#百度输入框的id为kw
+# driver.find_element_by_id("kw").send_keys("selenium")
+# driver.find_element_by_id("su").click()#百度按钮的id为su
+#
+# driver.quit()
+
+
+
+#其他常用方法
 from selenium import webdriver
 
-
 driver = webdriver.Chrome()
+driver.get("http://www.baidu.com")
 
-#访问百度首页
-first_url= 'http://www.baidu.com'
-print("now access %s" %(first_url))
-driver.get(first_url)
+# 获得输入框的尺寸
+size = driver.find_element_by_id('kw').size
+print(size)
 
-#访问新闻页面
-second_url='http://news.baidu.com'
-print("now access %s" %(second_url))
-driver.get(second_url)
+# 返回百度页面底部备案信息
+text = driver.find_element_by_id("cp").text
+print(text)
 
-#返回（后退）到百度首页
-print("back to  %s "%(first_url))
-driver.back()
+# 返回元素的属性值， 可以是 id、 name、 type 或其他任意属性
+attribute = driver.find_element_by_id("kw").get_attribute('type')
+print(attribute)
 
-#前进到新闻页
-print("forward to  %s"%(second_url))
-driver.forward()
+# 返回元素的结果是否可见， 返回结果为 True 或 False
+result = driver.find_element_by_id("kw").is_displayed()
+print(result)
 
 driver.quit()
+
